545c8dec39c337576f8d7a80f7885d345795a084
Thibaut <thibaut.collette@gmail.com>
2014-02-28 11:57:47 +0100

    Follow/unfollow works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,54 @@
 class User < ActiveRecord::Base
 	before_create :create_remember_token
 	before_save { self.email = email.downcase }
-	 
-has_many :microposts, dependent: :destroy
+
+	has_many :microposts, dependent: :destroy
+
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+
+
+	has_many :reverse_relationships, foreign_key: "followed_id",
+		class_name:  "Relationship",
+		dependent:   :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
+
 
 
 	validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+		uniqueness: { case_sensitive: false }
+
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
 	def User.new_remember_token
-    SecureRandom.urlsafe_base64
-  end
+		SecureRandom.urlsafe_base64
+	end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
 	def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
+		# This is preliminary. See "Following users" for the full implementation.
+		Micropost.where("user_id = ?", id)
+	end
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
 
-  private
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
 
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	private
+
+	def create_remember_token
+		self.remember_token = User.encrypt(User.new_remember_token)
+	end
 end
